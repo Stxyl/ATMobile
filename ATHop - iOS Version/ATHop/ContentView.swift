a75ab6751b446dce2827b17efeb29c67454bcefd
Stxyl <68010279+Stxyl@users.noreply.github.com>
2022-08-05 00:05:33 +1200

    UI Started
    Started working on a UI and making it Functional, the first screen is in place

--- ATHop - iOS Version/ATHop/ContentView.swift
+++ ATHop - iOS Version/ATHop/ContentView.swift
@@ -10,16 +10,37 @@ import MapKit
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            //implement background image
-            Image("Backgroundtemp")
-                .aspectRatio(contentMode: .fit)
-        }
         
-        //user prompts
+        
         VStack {
-            //TODO - Make info window
+            
+            //Main Title Text
+            Text("Pair your ATHop Card")
+                .offset(y: -25)
+                .font(.system(size: 27))
+                .bold()
+            
+            //icon and background
+            Image(systemName: "dot.radiowaves.left.and.right")
+                .font(.system(size: 76))
+                .offset(y: -5)
+            
+            //Secondary text
+            Text("Hold your card to the top of your iPhone")
+                .offset(y: 25)
+                .foregroundColor(.secondary)
+                .font(.system(size: 15))
+                
         }
+        .padding(.horizontal, 35)
+        .padding(.vertical, 50)
+        .background(.ultraThinMaterial)
+        .cornerRadius(20)
+        .ignoresSafeArea()
+        .background(
+            Image("Backgroundtemp")
+        )
+        .opacity(0.95)
         
     }
 }
