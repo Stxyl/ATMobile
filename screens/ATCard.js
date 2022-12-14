b26b82422ae9eaf708c4964a0b07eceec37f13e1
Stxyl <68010279+Stxyl@users.noreply.github.com>
2022-12-15 10:07:06 +1300

    Placeholders for NFC Functionality
    This is just a placeholder for getting NFC Functionalities working, NFC Does not work yet but working on a fix .

--- screens/ATCard.js
+++ screens/ATCard.js
@@ -1,23 +1,82 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { Button, StyleSheet, View, NativeModules, Text } from 'react-native';
+import * as SQLite from 'expo-sqlite';
+import * as NFC from 'react-native-nfc';
 
-export default function ATCard() {
+// Open a database connection
+const db = SQLite.openDatabase('db/cardinfo.db');
+
+const ATCard = (props) => {
+    // Declare a new state variable called "data" and initialize it to an empty string
+    const [data, setData] = useState('');
+    const startScan = () => {
+        // Check if the NFC object is available
+        if (NativeModules.NFC) {
+            // Add a listener to detect NFC tags
+            NativeModules.NFC.addListener((payload) => {
+                // Check if the detected NFC tag is a MIFARE DESFire Ev1 card
+                if (payload.type === 'MifareDesfire') {
+                    // Read the data from the card
+                    const data = NFC.parseMifareDesfire(payload.data);
+
+                    // Create the 'cards' table if it does not exist
+                    db.transaction((tx) => {
+                        tx.executeSql(
+                            'CREATE TABLE IF NOT EXISTS cards (card_data TEXT)',
+                            [],
+                            null,
+                            (error) => console.error(error)
+                        );
+                    });
+
+                    // Insert the data into the database
+                    db.transaction((tx) => {
+                        tx.executeSql(
+                            'INSERT INTO cards (card_data) VALUES (?)',
+                            [data],
+                            null,
+                            (error) => console.error(error)
+                        );
+                    });
+
+
+                    // Update the data variable with the data from the card
+                    setData(data);
+                    console.log(data);
+                }
+            });
+        }
+    }
+    // The ATCard component code goes here
+
     return (
         <View style={styles.container}>
-        <Text>ATCard Screen</Text>
-        <StatusBar style="auto" />
+            <Button
+                onPress={startScan}
+                title="Scan"
+            />
+            <Text style={styles.cardtext}>{data}</Text>
         </View>
     );
 }
 
+// Add this line to export the ATCard component
+export default ATCard;
+
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: '#fff',
         alignItems: 'center',
         justifyContent: 'center',
         backgroundColor: '#f4f5f0',
+        Width: '50%',
+        Height: '25%'
     },
+    cardtext: {
+        fontSize: 20,
+        fontWeight: 'bold',
+        color: '#000000',
+        textAlign: 'center',
+        marginTop: 20,
+    }
 });
-
